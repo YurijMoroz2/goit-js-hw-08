1b513238793f73aaddbc9c8e32f5d7ed645538e8
YurijMoroz2 <139611781+YurijMoroz2@users.noreply.github.com>
2023-11-26 18:27:26 +0200

    03 full homeWork-08

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,9 +1,9 @@
 // ----------------------------servis--------------------------------------
 const save = (key, value) => {
-    try {
-        const serializedState = JSON.stringify(value);
-        localStorage.setItem(key, serializedState);
-    } catch (error) {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
     console.error('Set state error: ', error.message);
   }
 };
@@ -16,55 +16,56 @@ const load = key => {
     console.error('Get state error: ', error.message);
   }
 };
+// ---------------------------------------------------------------------------------------
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 let emailInput = form.elements.email;
 let messageInput = form.elements.message;
+// console.log(emailInput);
 // // ----------------------------------------------------------------
 const FORM_KEY = 'feedback-form-state';
-let parseInfo = load(FORM_KEY)
 // // --------------------------------------------------------------------------
 
-// -------------------------------------------------------------------
-form.addEventListener('input', handleInput);
-function handleInput(event) {
-    event.preventDefault();
-    const email = event.target;
-    const message = event.target;
-    const formInfo = {
-        email: emailInput.value,
-        message: messageInput.value,
-    };
-        save(FORM_KEY, formInfo);
-    console.log("formInfo",formInfo)
-      };
-// ------------------------------------------------------------------------
-
-function auditInfo (){
-    if (parseInfo) {
-        emailInput.value = (load(FORM_KEY)).email
-        messageInput.value = (load(FORM_KEY)).message
+let parseInfo = load(FORM_KEY);
+console.log('parseInfo', parseInfo);
+function auditInfo() {
+  if (parseInfo) {
+    emailInput.value = load(FORM_KEY).email ?? '';
+    messageInput.value = load(FORM_KEY).message ?? '';
 
     //   messageInput.value =JSON.parse(localStorage.getItem(FORM_KEY)).message ?? '';
     //   emailInput.value = JSON.parse(localStorage.getItem(FORM_KEY)).email ?? '';
-      console.log('localeStorage__is__full');
-      } else {
-        console.log('LS_is_empty');
-        // emailInput.value = "";
-        // messageInput.value = "";
-    };
-};
-auditInfo()
+    console.log('localeStorage__is__full');
+  } else {
+    console.log('LS_is_empty');
+  }
+}
+auditInfo();
+// -------------------------------------------------------------------
+form.addEventListener('input', handleInput);
+function handleInput(event) {
+  event.preventDefault();
+  const formInfo = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  save(FORM_KEY, formInfo);
+  console.log('formInfo', formInfo);
+
+  auditInfo();
+}
+// ------------------------------------------------------------------------
+
 // // -----------------------------------------------------------------------
-form.addEventListener("submit",(evt)=>{evt.preventDefault();
-    if(parseInfo){
-                console.log("console",parseInfo)  
-        // emailInput.value = "";
-        // messageInput.value = "";  
-        localStorage.removeItem(FORM_KEY),
-        form.reset()};
-    }
-    )    
-    const throttledSaveFormInfo = throttle(handleInput, 500);
-    form.addEventListener('input', throttledSaveFormInfo);
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  let parseInfo = load(FORM_KEY);
+  if (parseInfo) {
+    console.log('console', parseInfo);
+
+    localStorage.removeItem(FORM_KEY), form.reset();
+  }
+});
+const throttledSaveFormInfo = throttle(handleInput, 500);
+form.addEventListener('input', throttledSaveFormInfo);
